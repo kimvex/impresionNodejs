16d58076317b1152b05078e79e32152799ddc449
Benjamin <chomi.numb.94@gmail.com>
2015-11-21 16:43:58 -0600

    Retiramos el modulo pty.js del proyecto y agregamos modulos nativos

--- impresion.js
+++ impresion.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var exec = require("child_process").exec;
 var pdf = require('html-pdf');
 var benjamin = "Benjamin De la cruz Martinez";
 var lugar = "Medellin y madero";
@@ -33,28 +34,20 @@ function crear(){
 escribir();
 
 function ejecutar(){
-	var pty = require('pty.js');
-
-
-	var term = pty.spawn('bash', [], {
-	  name: 'xterm-color',
-	  cols: 80,
-	  rows: 30,
-	  cwd: process.env.HOME,
-	  env: process.env
+	//term.write('cd '+__dirname+'\r');
+	command = 'lp chmsd.pdf';
+	child = exec(command, function(error, stdout, stderr){
+		if(error){
+			throw error;
+		}else{
+			console.log(stdout);
+		}
 	});
-
-	term.on('data', function(data) {
-	  console.log(data);
-	  borrado();
-	});
-
-	term.write('cd '+__dirname+'\r');
 	//term.write('touch kmv.docx\r');
 	//term.write('echo "CONSTANCIA DE GASTOS DE TRANSPORTE \n CICLO ESCOLAR 2015-2016 \n Comité Técnico del Programa Nacional de Becas para la\nEducación Superior Manutención en el Estado de Tabasco \nEl suscrit@ de nombre:'+benjamin+' \n con domicilo particular actual en:'+lugar+' de la localidad del municipio____ de del estado de_____ declaro bajo protesta de decir verdad, que no cuento con un vehiculo propio por lo que utilizo de transporte publico para trasladarme a la institucion donde realizo mis estudios univercitarios. Nombre de la Institucion Educativa: Instituto Técnologico de la Zona Olmeca. Direccion de la institucion educativa: \nCalle: Prolongacion de Ignacio Zaragoza S/N \nColonia: Centro  Localidad: Villa Ocuiltzapotlan \nMunicipio: Centro \nC.P.: 86270\nMensualmente mis gastos por transporte son por la cantidad de $___________(Monto en letra_____________).\nLo antes manifestado, es con la finalidad que se integre en mi expediente como aspirante al Programa Nacional de Becas para la Educación Superior Manutención en el Estado de Tabasco. ASi mismo, que lo expresado en esta carta son datos totalmente veridicos; por tal motivo en caso de incurrir en alguna falsedad aceopto la sancion o determinacion legal que me imponga el Comite Técnico, que puede consistir en la cancelacion de la misma y el reintegro del recurso que me fuera otorgado. De igual forma autorizo a la Institución de Educacion Superior y al Comite Tecnico para que verifiquen, en la forma en que se concidere conveniente, todos los datos acentados. (Anexo copia de los comprobantes que me proporcionan estos servicios).\n\n\nFECHA:___________________\n\nATENTAMENTE:\n\n\n_______________________________\nNOMBRE Y FIRMA\n" >> kmv.docx\r');
 	//term.write('ls\r');
 
-	term.write('lp chmsd.pdf\r');
+	//term.write('lp chmsd.pdf\r');
 }
 
 function borrado(){
